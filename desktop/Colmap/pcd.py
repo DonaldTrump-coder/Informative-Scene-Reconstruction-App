3f6e055aa0ecdeee8be7e84e19b41c7bd0e5aa42
DonaldTrump-coder <3279179450@qq.com>
2026-02-23 23:58:39 +0800

    add pcd parser

---
+++ desktop/Colmap/pcd.py
@@ -0,0 +1,5 @@
+class PCD:
+    def __init__(self, camera_file, image_file, point_file):
+        self.camera_file = camera_file
+        self.image_file = image_file
+        self.point_file = point_file
